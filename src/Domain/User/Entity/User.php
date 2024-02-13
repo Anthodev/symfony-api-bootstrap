222dc11d98f4ae86ba0ac81a2a4a4d4845313963
Cédric Anthony <Anthodev@users.noreply.github.com>
2024-02-13 22:46:52 +0100

    ✨ feat(user): added user & role entity/repository (#5)

---
+++ src/Domain/User/Entity/User.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\User\Entity;
+
+use App\Application\Common\Entity\EntityInterface;
+use App\Application\Common\Entity\Trait\IdTrait;
+use App\Application\Common\Entity\Trait\TimestampableTrait;
+use App\Domain\User\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'users')]
+#[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(fields: ['email'], message: 'This email is already used.')]
+#[UniqueEntity(fields: ['username'], message: 'This username is already used.')]
+class User implements EntityInterface, UserInterface, PasswordAuthenticatedUserInterface
+{
+    use IdTrait;
+    use TimestampableTrait;
+    use SoftDeleteableEntity;
+
+    #[Assert\NotBlank, Assert\Email]
+    #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
+    private string $email;
+
+    #[Assert\NotBlank, Assert\Length(min: 3, max: 255)]
+    #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
+    private string $username;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    private string $password;
+
+    private ?string $plainPassword;
+
+    private Role $role;
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $plainPassword): static
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    public function getRole(): Role
+    {
+        return $this->role;
+    }
+
+    public function setRole(Role $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        /** @var string $roleCode */
+        $roleCode = $this->role->getCode();
+
+        return [$roleCode];
+    }
+
+    public function eraseCredentials(): void
+    {
+        $this->plainPassword = null;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->getEmail();
+    }
+}
